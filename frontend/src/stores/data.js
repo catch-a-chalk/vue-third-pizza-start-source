2bb66eb0e5b396e82f33bae05f4293cc7265358a
Denis M <the.makushin@yandex.ru>
2024-11-23 14:40:39 +0300

    Создание состояний в хранилищах

--- frontend/src/stores/data.js
+++ frontend/src/stores/data.js
@@ -1,7 +1,24 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
+import doughJSON from "@/mocks/doughJSON";
+import {
+  normalizeDough,
+  normalizeIngredients,
+  normalizeSauces,
+  normalizeSize,
+} from "@/common/helpers/normalize";
+import ingredientsJSON from "@/mocks/ingredients.json";
+import saucesJSON from "@/mocks/sauces.json";
+import sizesJSON from "@/mocks/sizes.json";
+import miscJSON from "@/mocks/misc.json";
 
 export const useDataStore = defineStore('data', {
-  state: () => ({}),
+  state: () => ({
+    doughs: doughJSON.map(normalizeDough),
+    ingerdients: ingredientsJSON.map(normalizeIngredients),
+    sauces: saucesJSON.map(normalizeSauces),
+    sizes: sizesJSON.map(normalizeSize),
+    misc: miscJSON,
+  }),
   getters: {},
   actions: {},
 });
--- frontend/src/stores/pizza.js
+++ frontend/src/stores/pizza.js
@@ -1,7 +1,15 @@
 import { defineStore } from 'pinia'
 
 export const usePizzaStore = defineStore('pizza', {
-  state: () => ({}),
+  state: () => ({
+    index: null,
+    name: "",
+    sauceId: 0,
+    doughId: 0,
+    sizeId: 0,
+    sizeId: 0,
+    ingredients: [],
+  }),
   getters: {},
   actions: {},
 });
